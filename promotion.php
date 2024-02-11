38b5ce92518d6f07cced247b1c2a4af52cb9fc29
Realgorithm <hussaintabish0618@gmail.com>
2024-02-11 21:42:39 +0530

    error_page

--- promotion.php
+++ promotion.php
@@ -3,32 +3,10 @@
     <div class="card-body">
         <div class="row">
             <div class="col-md-12">
-            <div class="card mb-3">
+                <div class="card mb-3">
                     <div class="row g-0">
                         <div class="col-md-2">
-                            <img src="wp-content/uploads/sites/8/2022/11/logo1.png" class="img-fluid rounded-start"
-                                alt="...">
-                        </div>
-                        <div class="col-md-10">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-            <div class="card mb-3">
-                    <div class="row g-0">
-                        <div class="col-md-2">
-                            <img src="wp-content/uploads/sites/8/2022/11/logo1.png" class="img-fluid rounded-start"
-                                alt="...">
+                            <img src="wp-content/uploads/sites/8/2022/11/logo1.png" class="img-fluid rounded-start" alt="...">
                         </div>
                         <div class="col-md-10">
                             <div class="card-body">
@@ -48,8 +26,7 @@
                 <div class="card mb-3">
                     <div class="row g-0">
                         <div class="col-md-2">
-                            <img src="wp-content/uploads/sites/8/2022/11/logo1.png" class="img-fluid rounded-start"
-                                alt="...">
+                            <img src="wp-content/uploads/sites/8/2022/11/logo1.png" class="img-fluid rounded-start" alt="...">
                         </div>
                         <div class="col-md-10">
                             <div class="card-body">
@@ -66,24 +43,43 @@
         </div>
         <div class="row">
             <div class="col-md-12">
-            <div class="card mb-3">
-                <div class="row g-0">
-                    <div class="col-md-2">
-                        <img src="wp-content/uploads/sites/8/2022/11/logo1.png" class="img-fluid rounded-start"
-                            alt="...">
-                    </div>
-                    <div class="col-md-10">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural
-                                lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small>
-                            </p>
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-2">
+                            <img src="wp-content/uploads/sites/8/2022/11/logo1.png" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-10">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-2">
+                            <img src="wp-content/uploads/sites/8/2022/11/logo1.png" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-10">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
